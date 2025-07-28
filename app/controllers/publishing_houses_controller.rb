13c5a9e408fa1f97c2919761fda8d70aeb680111
Aldjia Boughias <boughias-aldjia@live.fr>
2025-07-28 14:17:47 +0200

    Add and style new/edit views for publishing houses

--- app/controllers/publishing_houses_controller.rb
+++ app/controllers/publishing_houses_controller.rb
@@ -1,12 +1,14 @@
 class PublishingHousesController < ApplicationController
-  before_action :set_publishing_house, only: %i[ show edit update destroy ]
+  before_action :set_publishing_house, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :authorize_user!, only: %i[edit update destroy]
 
-  # GET /publishing_houses or /publishing_houses.json
+  # GET /publishing_houses
   def index
     @publishing_houses = PublishingHouse.all
   end
 
-  # GET /publishing_houses/1 or /publishing_houses/1.json
+  # GET /publishing_houses/1
   def show
   end
 
@@ -19,52 +21,46 @@ class PublishingHousesController < ApplicationController
   def edit
   end
 
-  # POST /publishing_houses or /publishing_houses.json
+  # POST /publishing_houses
   def create
     @publishing_house = PublishingHouse.new(publishing_house_params)
+    @publishing_house.user = current_user
 
-    respond_to do |format|
-      if @publishing_house.save
-        format.html { redirect_to @publishing_house, notice: "Publishing house was successfully created." }
-        format.json { render :show, status: :created, location: @publishing_house }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @publishing_house.errors, status: :unprocessable_entity }
-      end
+    if @publishing_house.save
+      redirect_to @publishing_house, notice: "Maison d’édition créée avec succès."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /publishing_houses/1 or /publishing_houses/1.json
+  # PATCH/PUT /publishing_houses/1
   def update
-    respond_to do |format|
-      if @publishing_house.update(publishing_house_params)
-        format.html { redirect_to @publishing_house, notice: "Publishing house was successfully updated." }
-        format.json { render :show, status: :ok, location: @publishing_house }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @publishing_house.errors, status: :unprocessable_entity }
-      end
+    if @publishing_house.update(publishing_house_params)
+      redirect_to @publishing_house, notice: "Maison d’édition mise à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /publishing_houses/1 or /publishing_houses/1.json
+  # DELETE /publishing_houses/1
   def destroy
-    @publishing_house.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to publishing_houses_path, status: :see_other, notice: "Publishing house was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @publishing_house.destroy
+    redirect_to publishing_houses_path, status: :see_other, notice: "Maison d’édition supprimée."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_publishing_house
-      @publishing_house = PublishingHouse.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def publishing_house_params
-      params.require(:publishing_house).permit(:name, :description, :user_id)
+  def set_publishing_house
+    @publishing_house = PublishingHouse.find(params[:id])
+  end
+
+  def authorize_user!
+    unless @publishing_house.user == current_user
+      redirect_to publishing_houses_path, alert: "Accès refusé."
     end
+  end
+
+  def publishing_house_params
+    params.require(:publishing_house).permit(:name, :description, :logo)
+  end
 end
