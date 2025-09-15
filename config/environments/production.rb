9f4f2402c273dd9106381a00ad71e2be47df3fc1
Aldjia Boughias <boughias-aldjia@live.fr>
2025-09-15 10:37:01 +0200

    Update ActionMailer default sender to verified Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
   #
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "readnest-7b25107ffb5b.herokuapp.com", protocol: "https" }
-  config.action_mailer.default_options = { from: "boughias-aldjia@live.fr" }
+  config.action_mailer.default_options = { from: "boughias.aldjia@gmail.com" }
 
   config.action_mailer.smtp_settings = {
     user_name: "apikey",
