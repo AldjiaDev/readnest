b61210522246015f1a4165343137da57d467a4f4
Aldjia Boughias <boughias-aldjia@live.fr>
2025-09-26 14:23:20 +0200

    Add all bookshops section to home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @chronicles = Chronicle.order(created_at: :desc).limit(3)
-    @publishing_houses = PublishingHouse.limit(6)
+    @publishing_houses = PublishingHouse.order(created_at: :desc).limit(6)
+    @bookshops = Bookshop.order(created_at: :desc).limit(6)
   end
 end
