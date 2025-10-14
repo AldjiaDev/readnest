87ab9ecbbb2d8266b03281f03ff27f5a1c32984c
Aldjia Boughias <boughias-aldjia@live.fr>
2025-10-14 10:06:57 +0200

    feat(likes): add Turbo Stream support for real-time like/unlike on chronicle show page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,26 +1,29 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_chronicle
 
   def create
-    chronicle = Chronicle.find(params[:chronicle_id])
-    like = chronicle.likes.create(user: current_user)
+    @chronicle.likes.find_or_create_by(user: current_user)
 
-    if like.persisted?
-      Notification.create(
-        recipient: chronicle.user,
-        actor: current_user,
-        action: "liked",
-        notifiable: chronicle
-      )
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.turbo_stream
     end
-
-    redirect_back fallback_location: root_path
   end
 
   def destroy
-    chronicle = Chronicle.find(params[:chronicle_id])
-    like = chronicle.likes.find_by(user: current_user)
-    like.destroy if like
-    redirect_back fallback_location: root_path
+    @chronicle.likes.find_by(user: current_user)&.destroy
+
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_chronicle
+    @chronicle = Chronicle.find(params[:chronicle_id])
   end
 end
+
