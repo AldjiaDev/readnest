0f19e425cd7cbd193101a9d03d65b03354299081
Aldjia Boughias <boughias-aldjia@live.fr>
2025-10-15 13:34:43 +0200

    Improves SEO #26

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,22 +1,33 @@
-SitemapGenerator::Sitemap.default_host = "https://readnest-7b25107ffb5b.herokuapp.com"
+SitemapGenerator::Sitemap.default_host = "https://www.readnest.fr"
 
 SitemapGenerator::Sitemap.create do
   # Page d’accueil
   add root_path, priority: 1.0, changefreq: 'daily'
 
   # Chroniques
-  add chronicles_path, priority: 0.7, changefreq: 'daily'
+  add chronicles_path, priority: 0.8, changefreq: 'daily'
   Chronicle.find_each do |chronicle|
-    add chronicle_path(chronicle), lastmod: chronicle.updated_at, priority: 0.8
+    add chronicle_path(chronicle), lastmod: chronicle.updated_at, priority: 0.9
   end
 
-  # Profils utilisateurs
-  User.find_each do |user|
-    add user_path(user), priority: 0.5
+  # Librairies
+  Bookshop.find_each do |bookshop|
+    add bookshop_path(bookshop), lastmod: bookshop.updated_at, priority: 0.7
   end
 
   # Maisons d’édition
   PublishingHouse.find_each do |house|
-    add publishing_house_path(house), priority: 0.6
+    add publishing_house_path(house), lastmod: house.updated_at, priority: 0.7
   end
+
+  # Profils utilisateurs (si publics)
+  User.find_each do |user|
+    add user_path(user), lastmod: user.updated_at, priority: 0.4
+  end
+
+  # Page "À propos"
+  add about_path, priority: 0.5, changefreq: 'monthly'
+
+  # Recherche
+  add search_path, priority: 0.3, changefreq: 'weekly'
 end
