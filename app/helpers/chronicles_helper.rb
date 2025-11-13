51ff7681765d2f7bcc8de95d6ae759006be9a9c8
Aldjia Boughias <boughias-aldjia@live.fr>
2025-11-13 12:39:28 +0100

    Improve citation formatting: add automatic French quotes and italic blockquote styling

--- app/helpers/chronicles_helper.rb
+++ app/helpers/chronicles_helper.rb
@@ -1,2 +1,13 @@
 module ChroniclesHelper
+  def formatted_chronicle_content(text)
+    return "" if text.blank?
+
+    html = simple_format(h(text))
+
+    html = html.gsub(%r{<p>&gt;\s*(.+?)</p>}m) do
+      "<blockquote>#{$1}</blockquote>"
+    end
+
+    html.html_safe
+  end
 end
